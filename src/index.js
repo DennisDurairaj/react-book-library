b6b3a8f70a644d5fea06c18f38add09c79c68015
Dennis Durairaj <dennis.durairaj@gmail.com>
2018-06-28 17:22:40 +0200

    Login and logout functionality, jwt tokens

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import { Provider } from "react-redux";
 import registerServiceWorker from "./registerServiceWorker";
 import rootReducer from "./rootReducer";
 import "semantic-ui-css/semantic.min.css";
+import { userLoggedIn } from "./actions/auth";
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.JWT) {
+  const user = { token: localStorage.JWT };
+  store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
   <BrowserRouter>

c0d4477e96821998c67dd3a41271ae2b7c1a2161
Dennis Durairaj <dennis.durairaj@gmail.com>
2018-06-29 13:41:04 +0200

    Create signup page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage/HomePage";
 import LoginPage from "./components/pages/LoginPage/LoginPage";
 import DashboardPage from "./components/pages/DashboardPage/DashboardPage";
+import SignupPage from "./components/pages/SignupPage/SignupPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
 
@@ -15,6 +16,12 @@ const App = ({ location }) => {
         path="/login"
         exact
         component={LoginPage}
+      />
+      <GuestRoute
+        location={location}
+        path="/signup"
+        exact
+        component={SignupPage}
       />
       <UserRoute
         location={location}

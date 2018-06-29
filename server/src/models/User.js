921f0976326fd7e3248faa1f2ae141cfe305bbce
Dennis Durairaj <dennis.durairaj@gmail.com>
2018-06-29 15:14:47 +0200

    Sign up mechanism added

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import uniqueValidator from "mongoose-unique-validator";
 
 const schema = new mongoose.Schema(
   {
-    email: { type: String, required: true, lowercase: true, index: true },
-    passwordHash: { type: String, required: true }
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+      unique: true
+    },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, default: false }
   },
   { timestamps: true }
 );
@@ -26,8 +34,15 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
+    confirmed: this.confirmed,
     token: this.generateJWT()
   };
 };
+
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+schema.plugin(uniqueValidator, { message: "Email already exists" });
 
 export default mongoose.model("User", schema);

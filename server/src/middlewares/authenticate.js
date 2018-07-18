1e2e375d43a3e376d499095bc5335e521a9fbe04
Dennis Durairaj <dennis.durairaj@gmail.com>
2018-07-18 15:34:51 +0200

    Authentication for books api

---
+++ server/src/middlewares/authenticate.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  const header = req.headers.authorization;
+  let token;
+  if (header) token = header.split(" ")[1];
+
+  if (token) {
+    jwt.verify(token, proess.env.JWT_TOKEN, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ errors: { global: "Invalid token" } });
+      } else {
+        req.userEmail = decoded.email;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ errors: { global: "No token" } });
+  }
+};
